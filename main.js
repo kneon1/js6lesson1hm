82eb8f5a504c954a9c25f9745d606c92ef9854a3
neon <neon@mail.com>
2023-03-18 23:40:28 +0900

    v1

---
+++ main.js
@@ -0,0 +1,45 @@
+let user = {
+    user1:{
+        name: prompt('введите имя'),
+        age: prompt('введите возраст')
+    },
+    user2:{
+        name: prompt('введите имя'),
+        age: prompt('введите возраст')
+    },
+    user3:{
+        name: prompt('введите имя'),
+        age: prompt('введите возраст')
+    },
+    user4:{
+        name: prompt('введите имя'),
+        age: prompt('введите возраст')
+    },
+    user5:{
+        name: prompt('введите имя'),
+        age: prompt('введите возраст')
+    },
+    user6:{
+        name: prompt('введите имя'),
+        age: prompt('введите возраст')
+    },
+    user7:{
+        name: prompt('введите имя'),
+        age: prompt('введите возраст')
+    },
+    user8:{
+        name: prompt('введите имя'),
+        age: prompt('введите возраст')
+    },
+    user9:{
+        name: prompt('введите имя'),
+        age: prompt('введите возраст')
+    },
+    user10:{
+        name: prompt('введите имя'),
+        age: prompt('введите возраст')
+    }
+}
+for(let key in user){
+console.log(user[key]);
+}
